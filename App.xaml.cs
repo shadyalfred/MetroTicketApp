ef70d836f7ecbcda00813022e075f08a6cafdb20
Shady Alfred <shadyalfred98@gmail.com>
2025-11-17 01:24:21 +0200

    `fix` always use dark theme

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+        Current!.UserAppTheme = AppTheme.Dark;
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
